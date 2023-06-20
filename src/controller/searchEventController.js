5bbd452b344896392082a4bfd478e92c90c2ec0e
Breeze Artist <Breezartist29@gmail.com>
2023-06-20 16:03:45 +0700

    category

--- src/controller/searchEventController.js
+++ src/controller/searchEventController.js
@@ -3,12 +3,16 @@ const { Event, EventCategory } = require("../models");
 exports.getEventByCategory = async (req, res, next) => {
     try {
         const { input } = req.params;
-        console.log("--->", input);
-        const event = await EventCategory.findAll({
-            where: { name: input },
-            include: [{ model: Event }],
-        });
-        res.json(event);
+        if (input == "all") {
+            const event = await Event.findAll();
+            res.status(200).json(event);
+        } else {
+            const event = await EventCategory.findAll({
+                where: { name: input },
+                include: [{ model: Event }],
+            });
+            res.status(200).json(event);
+        }
     } catch (err) {
         next(err);
     }
