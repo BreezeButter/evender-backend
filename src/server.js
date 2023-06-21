d21fd9ffedc44f2ef2b382875b04047e558e95bf
jaishinhong <jai753198@gmail.com>
2023-06-21 17:33:34 +0700

    chatroom

--- src/server.js
+++ src/server.js
@@ -8,16 +8,39 @@ const io = require("socket.io")(httpServer, {
     },
 });
 
-io.on("connection", (socket) => {
-    console.log("A user connected.");
+// io.on("connection", (socket) => {
+//     console.log("A user connected.");
 
-    socket.on("message", (message) => {
-        console.log("Received message:", message);
-        io.emit("message", message);
+//     socket.on("disconnect", () => {
+//         console.log("A user disconnected.");
+//     });
+
+//     socket.on("message", (message) => {
+//         io.emit("message", message);
+//     });
+// });
+
+const chatRooms = {}; // Store the chat rooms
+
+io.on("connection", (socket) => {
+    console.log("New client connected");
+
+    // Handle joining a chat room
+    socket.on("join room", (room) => {
+        socket.join(room); // Join the specified room
+        chatRooms[room] = chatRooms[room] || []; // Create the room if it doesn't exist
+        socket.emit("room messages", chatRooms[room]); // Send existing messages to the joining user
+    });
+
+    // Handle chat messages
+    socket.on("chat message", ({ room, message }) => {
+        console.log("Message received:", message);
+        chatRooms[room].push(message); // Store the message in the room's message array
+        io.to(room).emit("chat message", message); // Broadcast the message to all clients in the room
     });
 
     socket.on("disconnect", () => {
-        console.log("A user disconnected.");
+        console.log("Client disconnected");
     });
 });
 
