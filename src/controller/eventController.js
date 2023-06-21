85a46a4f2aa9ec9a4bf893bcdabcaa56ffc91a26
jaishinhong <jai753198@gmail.com>
2023-06-22 02:01:45 +0700

    createEvent

--- src/controller/eventController.js
+++ src/controller/eventController.js
@@ -1,7 +1,9 @@
 const { Event, JoinEventUser, User } = require("../models");
+const cloudinary = require("../config/cloudinary");
 
 exports.getAllEvents = async (req, res, next) => {
     try {
+        console.log("hello");
         const events = await Event.findAll({
             include: { model: JoinEventUser, include: User },
         });
@@ -14,6 +16,12 @@ exports.getAllEvents = async (req, res, next) => {
 
 exports.createEvent = async (req, res, next) => {
     try {
+        const result1 = cloudinary.uploader.upload(req.files[0].path);
+        const result2 = cloudinary.uploader.upload(req.files[1].path);
+        const result3 = cloudinary.uploader.upload(req.files[2].path);
+        const resultAll = await Promise.all([result1, result2, result3]);
+
+        res.status(200).json(resultAll[0]);
     } catch (err) {
         next(err);
     }
--- src/routes/eventRoute.js
+++ src/routes/eventRoute.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const eventController = require("../controller/eventController");
+const authenticate = require("../middlewares/authenticate");
+const upload = require("../middlewares/upload");
 
 const router = express.Router();
 
 router.get("/getAllEvents", eventController.getAllEvents);
-router.post("/createEvent", eventController.createEvent);
+router.post("/createEvent", upload.array("image"), eventController.createEvent);
 module.exports = router;
