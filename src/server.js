a750ba6f056acf7a416b9c3b4fa4ebbf01cd5240
jaishinhong <jai753198@gmail.com>
2023-06-24 11:45:39 +0700

    chat

--- src/server.js
+++ src/server.js
@@ -1,16 +1,14 @@
 const server = require("./app");
 const { Server } = require("socket.io");
-const { instrument } = require("@socket.io/admin-ui");
+
 const io = new Server(server, {
     cors: {
-        origin: ["https://admin.socket.io", "http://localhost:5173"],
+        origin: ["http://localhost:5173"],
         credentials: true,
         methods: ["GET", "POST"],
     },
 });
-instrument(io, {
-    auth: false,
-});
+
 const onlineUser = {};
 io.use((socket, next) => {
     const userId = socket.handshake.auth.id;
@@ -22,7 +20,7 @@ io.use((socket, next) => {
 });
 
 io.on("connection", (socket) => {
-    socket.on("join_room", function (roomName) {
+    socket.on("joinRoom", function (roomName) {
         socket.join(roomName);
         console.log(socket.rooms, "1");
     });
@@ -37,3 +35,43 @@ io.on("connection", (socket) => {
 
 const port = process.env.PORT || 8080;
 server.listen(port, () => console.log(`server running on port: ${port}`));
+
+//#############################################################################
+
+// const server = require("./app");
+// const { Server } = require("socket.io");
+
+// const io = new Server(server, {
+//     cors: {
+//         origin: ["http://localhost:5173"],
+//         credentials: true,
+//         methods: ["GET", "POST"],
+//     },
+// });
+
+// const onlineUser = {};
+// io.use((socket, next) => {
+//     const userId = socket.handshake.auth.id;
+//     console.log(socket.id);
+//     console.log(userId);
+//     onlineUser[userId] = socket.id;
+//     console.log(onlineUser);
+//     next();
+// });
+
+// io.on("connection", (socket) => {
+//     socket.on("joinRoom", function (roomName) {
+//         socket.join(roomName);
+//         console.log(socket.rooms, "1");
+//     });
+
+//     socket.on("sendMessage", (input) => {
+//         console.log(input);
+//         socket.to(input.room).emit("receiveMessage", input.message);
+//     });
+
+//     socket.on("disconnect", () => {});
+// });
+
+// const port = process.env.PORT || 8080;
+// server.listen(port, () => console.log(`server running on port: ${port}`));
