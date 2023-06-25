81bd68ad61f6ce62eacc18c1bb4ea73c7c528a85
Breeze Artist <Breezartist29@gmail.com>
2023-06-25 23:27:39 +0700

    ok

--- src/controller/searchEventController.js
+++ src/controller/searchEventController.js
@@ -1,6 +1,5 @@
 const { Event } = require("../models");
 const { Op } = require("sequelize");
-const { QueryTypes } = require("sequelize");
 
 exports.getSearch = async (req, res, next) => {
     try {
@@ -8,7 +7,7 @@ exports.getSearch = async (req, res, next) => {
 
         let whereOp = {};
         let whereOptitle = {};
-        let whereDate = {};
+        let whereCondition = {};
 
         if (value.eventCategoryId) {
             whereOp.eventCategoryId = +value.eventCategoryId;
@@ -17,25 +16,32 @@ exports.getSearch = async (req, res, next) => {
             whereOp.placeProvince = value.placeProvince;
         }
         if (value.dateStart) {
-            whereDate.dateStart = value.dateStart;
+            whereCondition.date_start = {
+                [Op.gte]: new Date(value.dateStart),
+            };
         }
         if (value.dateEnd) {
-            whereDate.dateEnd = value.dateEnd;
+            whereCondition.date_end = {
+                [Op.lte]: new Date(value.dateEnd),
+            };
         }
         if (value.box) {
-            whereOptitle.placeName = { [Op.like]: `%${value.box}%` };
-        }
-        if (value.box) {
-            whereOptitle.title = { [Op.like]: `%${value.box}%` };
+            whereOptitle = {
+                [Op.or]: [
+                    { placeName: { [Op.like]: `%${value.box}%` } },
+                    { title: { [Op.like]: `%${value.box}%` } },
+                ],
+            };
         }
 
-        console.log("whereOp", whereOp);
-        console.log("whereOptitle", whereOptitle);
-        console.log("whereDate", whereOptitle);
+        const whereQuery = {
+            ...whereOp,
+            ...whereOptitle,
+            ...whereCondition,
+        };
 
         const searchOutput = await Event.findAll({
-            where: whereOp,
-            [Op.or]: whereOptitle,
+            where: whereQuery,
         });
 
         res.status(200).json(searchOutput);
--- src/routes/searchRoute.js
+++ src/routes/searchRoute.js
@@ -3,7 +3,7 @@ router = express.Router();
 
 const searchEventController = require("../controller/searchEventController");
 
-router.get("/filter", searchEventController.getSearch);
+router.post("/filter", searchEventController.getSearch);
 router.get("/place", searchEventController.palaceProvince);
 
 module.exports = router;
