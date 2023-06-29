1c226a0c9e50c8ce84d506b09644c75856383c84
bryce-brny <brightbrgh@gmail.com>
2023-06-29 12:28:56 +0700

    ok

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const { Chat } = require("./models");
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173"],
+        origin: "*",
         credentials: true,
         methods: ["GET", "POST"],
     },
